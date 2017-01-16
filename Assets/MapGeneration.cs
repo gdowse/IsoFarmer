baa738f436e8ae46e0ae57603f81ec2316f35c41
Dowse <gregory.dowse@ldngol-nmxd2bu.emea.corp.ipgnetwork.com>
2017-01-16 16:43:17 +0000

    Initialize Project

---
+++ Assets/MapGeneration.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGeneration : MonoBehaviour {
+
+	public GameObject[] TileArray = new GameObject[20];
+	// Use this for initialization
+	void Start () {
+		GenerateMap ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GenerateMap(){
+		for(int y = 0; y < 20; y++){
+			for(int x = 0; x < 20; x++){
+				Instantiate(TileArray[0], new Vector3((-0.5F * y) + (0.5F * x),(x * -0.25F),0), Quaternion.identity);
+			}
+		}
+	}
+}
